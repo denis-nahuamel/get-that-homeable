281d446eab0831d52b2134cc0dd44df56a586cdf
Denis <nahuamel.denis@gmail.com>
2022-07-11 21:47:27 -0500

    add styles

---
+++ src/styles/form.js
@@ -0,0 +1,112 @@
+import { contCenterCenter, contColumn, contColumnStart, contRowCenter, contRowGap } from "./utils"
+
+export const inputBasic = `
+padding: 8px;
+gap: 8px;
+height: 30px;
+background: #FFFFFF;
+border-radius: 8px;
+`
+// background: url("https://static.thenounproject.com/png/101791-200.png") no-repeat scroll 5px 11px;
+    
+export const inputForm = `
+    ${inputBasic};
+    height: 25px;
+    border: 1px solid #F48FB1;
+`
+export const input = `
+    ${inputBasic};
+    padding-left: 30px;
+    background: url("https://static.thenounproject.com/png/101791-200.png") no-repeat scroll 5px 11px;
+    background-size: 26px;
+    border: 1px solid #F48FB1;
+    width: 600px;
+`
+export const anotherInput = `
+    ${inputBasic};
+    border: 1px solid #F48FB1;
+    height: 21px;
+    `
+export const textArea = `
+    ${anotherInput};
+    width: 600px;
+    height: 76px;
+
+`
+// -webkit-appearance: none;
+export const checkbox = `
+    border: 1px solid #F48FB1;  
+    width: 20px;
+    height: 20px;
+`
+export const select = `
+    ${inputBasic};
+    border: 1px solid #F48FB1;
+    height: 40px;
+`
+export const labelInputCont = `
+    ${contColumnStart};
+    gap:8px;
+    margin:8px;
+`
+export const labelForm = `
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 10px;
+    line-height: 12px;
+    letter-spacing: 1.5px;
+    text-transform: uppercase;
+    color: #373737;
+`
+export const labelOptions = `
+    ${contRowGap};
+    margin:8px;
+    `
+    export const labelCheck = `
+    ${contRowCenter};
+    margin:8px;
+
+`
+export const thumbImages = `
+    margin: 8px; 
+    display: flex;
+    align-items: center;
+    width: 600px;
+    height: 136px;
+    left: 0px;
+    top: 824px;
+
+    /* Background */
+
+    background: #F5F5F6;
+`
+export const thumbImage = `
+${contCenterCenter}
+width: 120px;
+height: 120px;
+margin-left: 8px;
+/* BackgroundLight */
+
+background: #E1E2E1;
+border-radius: 8px;
+`
+// width: 274px;
+export const sendButton = `
+gap: 8px;
+margin: 16px;
+width: 90%;
+height: 40px;
+
+border: 0px;
+    color: white;
+
+background: #F48FB1;
+border-radius: 16px;
+`
+export const signUpContainer = `
+    ${contColumn};
+    margin: 0 200px;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
+    border-radius: 8px;
+`
---
+++ src/styles/typography.js
@@ -0,0 +1,103 @@
+export const sansDefault = `
+    font-family: 'Source Sans Pro';
+    font-style: normal;
+    font-weight: 600;
+    line-height: 28px;
+`
+export const sansGray = `
+    ${sansDefault};
+    color: #333333;
+`
+export const title = `
+    ${sansGray};
+    font-size: 22px;
+`
+export const sans22W600 = `
+    ${sansGray};
+    font-size: 22px;
+`
+export const titleCheck = `
+    ${sansGray};
+    font-size: 16px;
+`
+export const description = `
+    ${sansGray};
+    font-size: 18px;
+`
+export const sans18W600 = `
+    ${sansGray};
+    font-size: 18px;
+`
+export const sans14W600 = `
+    ${sansGray};
+    font-size: 14px;
+`
+export const sans14W600Gray = `
+    ${sans14W600}
+    color: #B8B8BB;
+`
+export const sans18W400 = `
+    ${sansGray};
+    font-weight: 400;
+    font-size: 18px;
+`
+export const sans28 = `
+    ${sansGray};
+    font-size: 28px;
+`
+export const subTitle = `
+    ${sansDefault};
+    font-size: 22px;
+    color: #FA4A0C;
+`
+export const headText = `
+    font-style: normal;
+    font-weight: 400;
+    font-size: 17px;
+    line-height: 20px;
+    text-align: center;
+    color: #9A9A9D;
+`
+export const Montserrat = `
+    font-family: 'Montserrat';
+    font-style: normal;
+`
+export const montW500S20 = `
+    ${Montserrat};
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 28px;
+    /* identical to box height, or 140% */
+
+    letter-spacing: 0.15px;
+
+    /* DarkGray */
+
+    color: #373737;
+`
+export const montW400S24 = `
+    ${Montserrat};
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 28px;
+    /* identical to box height, or 140% */
+
+    letter-spacing: 0.15px;
+
+    /* DarkGray */
+
+    color: #373737;
+`
+export const montW400S36= `
+    ${Montserrat};
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 28px;
+    /* identical to box height, or 140% */
+
+    letter-spacing: 0.15px;
+
+    /* DarkGray */
+
+    color: #373737;
+`
