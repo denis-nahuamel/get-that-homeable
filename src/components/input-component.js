1332ee4ae95269967d19523ab32d24823ecc7764
Denis <nahuamel.denis@gmail.com>
2022-07-11 21:47:48 -0500

    add input component

---
+++ src/components/input-component.js
@@ -0,0 +1,17 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { input, inputForm, labelForm, labelInputCont } from "../styles/form";
+
+const InputComponent = ({labelText,idInput, placeholder, type,width }) => {
+    return (
+        <div css={css`${labelInputCont}`}>
+                <label css={css`${labelForm}`}>{labelText}</label>
+                <input css={css`${inputForm}; width: ${width?width:'90%' };`}
+                 type={type} 
+                 id={idInput} 
+                 name = {idInput} 
+                 placeholder={placeholder}/>
+        </div>
+    )
+}
+export default InputComponent;
