ba3f0c0686f4d365241dc0843920d725d2e84ca2
Denis <nahuamel.denis@gmail.com>
2022-07-13 17:20:14 -0500

    add get properties

--- src/services/property-service.js
+++ src/services/property-service.js
@@ -3,3 +3,6 @@ import apiFetch from "./api-fetch"
 export const addProperty = (property) => {
     return apiFetch("/properties", {body: property}).then(response => {return response})
  }
+export const getProperties = () => {
+    return apiFetch("/properties").then(response => {return response})
+}
