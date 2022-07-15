5b8b3e2bb243d8b0d6d06dcd9f50322fc0484228
Denis <nahuamel.denis@gmail.com>
2022-07-15 10:16:08 -0500

    partial filters

--- src/components/property-components/options-content.js
+++ src/components/property-components/options-content.js
@@ -2,36 +2,71 @@
 import {css} from "@emotion/react";
 import { ThemeProvider } from "@emotion/react"
 import { ToggleButton, ToggleButtonGroup } from "@mui/material"
+import { useState } from "react";
 import { toggle } from "../../styles/form"
 import { contColumn, contRow } from "../../styles/utils";
 import { theme } from "../themes"
 
-export const PriceContent = () => {
+export const PriceContent = ({onFilterParams}) => {
+    const handleFilterPrice = (event) => {
+        event.preventDefault();
+        let values= event.target.elements;
+        onFilterParams({
+            min_price: values.min_price.value, 
+            max_price: values.max_price.value, 
+        })
+    }
     return (
         <div>
             <div>SALARY RANGE</div>
-            <div>
-                <input placeholder="min"></input> - 
-                <input placeholder="max"></input>
-            </div>
-            <div><button>DONE</button></div>
+            <form onSubmit={handleFilterPrice}>
+                <div>
+                    <input placeholder="min" id="min_price" name="min_price"></input> - 
+                    <input placeholder="max" id="max_price" name="max_price"></input>
+                </div>
+                <div><button type="submit">DONE</button></div>
+            </form>
         </div>
     )
 }
-export const PropertyContent = () => {
+export const PropertyContent = ({onFilterParams}) => {
+    const handleFilterPrice = (event) => {
+        event.preventDefault();
+        let values= event.target.elements;
+        onFilterParams({
+            property_type_apartment: values.apartment.checked===true?"apartment":null, 
+            property_type_house: values.house.checked===true?"house":null
+        })
+    }
     return (
         <div>
             <div>PROPERTY TYPE</div>
-            <div>
-                <input type="checkbox"/> Houses
-                <input type="checkbox"/> Apartments 
-                
-            </div>
-            <div><button>DONE</button></div>
+            <form onSubmit={handleFilterPrice}>
+                <div>
+                    <input type="checkbox" id="house" name="house"/> Houses
+                    <input type="checkbox" id="apartment" name="apartment"/> Apartments 
+                    
+                </div>
+                <div><button type="submit">DONE</button></div>
+            </form>
         </div>
     )
 }
-export const BedsBathsContent = () => {
+export const BedsBathsContent = ({onFilterParams}) => {
+    const [bed, setBed] = useState("0");
+    const handleBed = (_event, value) => {
+        setBed(value)
+        onFilterParams({
+            bedrooms: value 
+        })
+    }
+    const [bath, setBath] = useState("0");
+    const handleBath = (_event, value) => {
+        setBath(value)
+        onFilterParams({
+            bathrooms: value 
+        })
+    }
     return (
         <div>
             <div css={css`${contColumn}`}>
@@ -41,9 +76,11 @@ export const BedsBathsContent = () => {
                             color="pink"
                             exclusive
                             aria-label="text alignment"
+                            value={bed}
+                            onChange={handleBed}
                             >
                             <ToggleButton
-                            css={css`${toggle}`} value="rent" aria-label="left aligned">
+                            css={css`${toggle}`} value="0" aria-label="left aligned">
                                 Any
                             </ToggleButton>
                             <ToggleButton css={css`${toggle}`} value="1" aria-label="centered">
@@ -68,9 +105,11 @@ export const BedsBathsContent = () => {
                             color="pink"
                             exclusive
                             aria-label="text alignment"
+                            value={bath}
+                            onChange={handleBath}
                             >
                             <ToggleButton
-                            css={css`${toggle}`} value="rent" aria-label="left aligned">
+                            css={css`${toggle}`} value="0" aria-label="left aligned">
                                 Any
                             </ToggleButton>
                             <ToggleButton css={css`${toggle}`} value="1" aria-label="centered">
@@ -95,9 +134,9 @@ export const BedsBathsContent = () => {
         </div>
     )
 }
-const ReturnContent = ({type})=>{
-    if(type==="price") return <PriceContent />
-    if(type==="property_type") return <PropertyContent />
-    if(type==="beds_baths") return <BedsBathsContent />
+const ReturnContent = ({type, onFilterParams})=>{
+    if(type==="price") return <PriceContent onFilterParams={onFilterParams} />
+    if(type==="property_type") return <PropertyContent onFilterParams={onFilterParams}/>
+    if(type==="beds_baths") return <BedsBathsContent onFilterParams={onFilterParams}/>
 }
 export default ReturnContent;
