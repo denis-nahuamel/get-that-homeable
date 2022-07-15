9c0606e4f411fec686c7ef7daf8e9c7730c5d0d2
lheredias <herediasotelo@gmail.com>
2022-07-15 03:55:27 -0500

    add query strings

--- src/services/property-service.js
+++ src/services/property-service.js
@@ -1,8 +1,28 @@
 import apiFetch from "./api-fetch"
 
-export const addProperty = (property) => {
+export const addProperty = (property, queryParams) => {
     return apiFetch("/properties",  {body: property, property_creator: true}).then(response => {return response})
  }
-export const getProperties = () => {
-    return apiFetch("/properties").then(response => {return response})
+
+export const getProperties = ({ address, min_price, max_price, min_area, max_area, pets, page,
+    bedrooms, bathrooms, operation_rent, operation_sale, property_type_apartment, property_type_house}) => {
+    const endpoint = `/properties?` + 
+    `address${address ? "="+address : ""}` + 
+    `&min_price${min_price ? "="+min_price : ""}` +
+    `&max_price${max_price ? "="+max_price : ""}` +
+    `&min_area${min_area ? "="+min_area : ""}` +
+    `&max_area${max_area ? "="+max_area : ""}` +
+    `&pets${pets ? "="+pets : ""}` +
+    `&page${page ? "="+page : ""}` +
+    `&bedrooms${bedrooms ? "="+bedrooms : ""}` +
+    `&bathrooms${bathrooms ? "="+bathrooms : ""}` +
+    `&operation%5B%5D${operation_rent ? "="+operation_rent : ""}` +
+    `&operation%5B%5D${operation_sale ? "="+operation_sale : ""}` +
+    `&property_type%5B%5D${property_type_apartment ? "="+property_type_apartment : ""}` +
+    `&property_type%5B%5D${property_type_house ? "="+property_type_house : ""}`
+
+    return apiFetch(endpoint).then(response => {
+        console.log(endpoint)
+        return response
+    })
 }
