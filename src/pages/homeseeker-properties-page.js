6fb96e44dca9382f53b04fce2820576cea511450
lheredias <herediasotelo@gmail.com>
2022-07-20 20:00:30 -0500

    add fav and contact tabs

--- src/pages/homeseeker-properties-page.js
+++ src/pages/homeseeker-properties-page.js
@@ -16,6 +16,8 @@ const HomeseekerPropertiesPage = ()=> {
     const {user} = useAuth();
     const [properties, setProperties] = useState(null)
     const {savedProperties, getPropertyContext} = useSave();
+    const [section, setSection] = useState("favorites")
+
 
     // useEffect(()=>{
     //     getSavedProperties().then(response => {           
@@ -25,17 +27,75 @@ const HomeseekerPropertiesPage = ()=> {
 
     // },[])
     useEffect(()=>{
+        let filteresSavedProperties = null        
         if (savedProperties === null) {
-            getPropertyContext()            
-        } 
-        setProperties(savedProperties?.map(item=>item.property))
+            getPropertyContext()    
+        } else {
+            filteresSavedProperties = [...savedProperties]
+        }
+        if (section === "favorites") {
+            filteresSavedProperties = savedProperties?.filter(savedProperty => savedProperty.favorite === true)
+        } else {
+            filteresSavedProperties = savedProperties?.filter(savedProperty => savedProperty.contacted === true)
+        }
+        setProperties(filteresSavedProperties?.map(item=>item.property))
 
-    },[user, savedProperties])
-   
+    },[user, savedProperties, section])
+    
+    const StyledButton = css`
+        background: none;
+        color: inherit;
+        border: none;
+        disabled: true;
+        padding: 0;
+        font: inherit;
+        cursor: pointer;
+        outline: inherit;
+        padding: 0.3rem;
+        &:hover {
+            opacity: 70%;
+          };
+        &:disabled {
+            cursor: not-allowed;
+            pointer-events: none;
+            border-bottom: 3px solid #F48FB1;
+        }
+    `;
+    
+    const styledDiv = css`
+        gap: 1rem;
+        display: flex;
+        margin-left: 5rem;
+    `;
+
+    function onFavorites(event) {
+        setSection("favorites")
+    }
+    function onContacted(event) {
+        setSection("contacted")
+    }
     return (
         <>
         {user?.user_type === "homeseeker" ? (
-            (properties ? <ListProperties properties = {properties} /> :
+            (properties ? 
+            (<>
+            <div css={styledDiv}>
+                <button 
+                    css={StyledButton} 
+                    disabled={section === "favorites" ? true : false}
+                    onClick={onFavorites}>
+                        Favorites
+                </button>
+                <button 
+                    css={StyledButton} 
+                    disabled={section === "contacted" ? true : false}
+                    onClick={onContacted}>
+                        Contacted
+                </button>
+            </div>
+                <ListProperties properties = {properties} section={section} />
+            </>
+            ) :
             "You don't have any saved properties")
         ) : "You must be logged in as a homeseeker in order to save properties"
         }
